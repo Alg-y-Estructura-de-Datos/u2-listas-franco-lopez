c55d3cd3b755ff17c88896a7b384c22e61144f84
franco2355 <142048169+franco2355@users.noreply.github.com>
2024-08-27 21:55:01 -0300

    Update Ej2.cpp

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include "Lista/Lista.h"
 using namespace std;
+void print3(Lista <int> &vec, int n, int el){
+    for (int i = 0; i < n; i++){
+        if (vec.getDato(i) == el){
+            vec.remover(i); 
+        }
+    }
+}
 
 int main() {
-
-    cout << "Ejercicio N° 2" << endl;
-
+    Lista <int> vec; 
+    int num, n, el; 
+    cout << "Ejercicio N° 1" << endl;
+    cout << "Ingrese el tamaño de la lista: "; 
+    cin >> n; 
+    cout << "Ingrese los valores del vector: " << endl ; 
+    for (int i = 0; i < n; i++){
+        cin >> num; 
+        vec.insertarUltimo(num); 
+    }
+    cout << "Ingrese numero a eliminar: ";
+    cin >> el; 
+    cout << "Lista despues de eliminar: " << endl; 
+    print3(vec, n, el); 
+    vec.print(); 
     return 0;
 }
